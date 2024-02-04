0a509b749590a01fcfb9fb511098276acdd7dd94
Pratham Anil Tetgure <89979888+Prattycodes@users.noreply.github.com>
2024-02-04 07:58:46 +0000

    JS Datatypes

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict" 
+// alert(3+3)
+console.log(3+3);
+console.log("Pratham");
+
+let name = "Pratham"
+let age = 21
+let isLoggedIn = false
+let state;
+
+// number Range => 2 to power 53
+// bigint - Mostly used in stock market 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique (Used in majorly ReactJS)
+
+//Object 
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
